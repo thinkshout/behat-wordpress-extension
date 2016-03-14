32809bbe77bff2952e154ff35b62285a79aa5647
Paul Gibbs <djpaul@gmail.com>
2016-03-14 17:51:41 +0000

    Remove a bunch of stuff from old approach

--- src/Context/Initializer/WordPressContextInitializer.php
+++ src/Context/Initializer/WordPressContextInitializer.php
@@ -19,38 +19,10 @@ class WordPressContextInitializer implements ContextInitializer
         $this->wordpressParams = $wordpressParams;
     }
 
-    /**
-     * setup the wordpress environment / stack if the context is a wordpress context
-     *
-     * @param Context $context
-     */
     public function initializeContext(Context $context)
     {
         if (!$context instanceof WordPressContext) {
             return;
         }
-
-        $this->prepareEnvironment();
-        $this->loadWordPress();
-    }
-
-    protected function prepareEnvironment()
-    {
-        $parts                      = parse_url($this->minkParams['base_url']);
-        $_SERVER['HTTP_HOST']       = $parts['host'] . (isset($parts['port']) ? ':' . $parts['port'] : '');
-        $_SERVER['SERVER_PROTOCOL'] = 'HTTP/1.1';
-        $PHP_SELF                   = $GLOBALS['PHP_SELF'] = $_SERVER['PHP_SELF'] = '/index.php';
-    }
-
-    protected function loadWordPress()
-    {
-        $wordpress = $this->wordpressParams['path'] . '/wp-load.php';
-
-        if (!file_exists($wordpress))
-        {
-            return;
-        }
-
-        require_once $wordpress;
     }
 }
