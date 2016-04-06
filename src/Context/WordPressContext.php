b87012319b8faf4558eedf0973d0532324d51b4f
Paul Gibbs <djpaul@gmail.com>
2016-04-06 23:17:22 +0100

    When initialising context, store reference to initialiser (and vice-vetsa).

--- src/Context/WordPressContext.php
+++ src/Context/WordPressContext.php
@@ -1,66 +1,20 @@
 <?php
-
-use Behat\Gherkin\Node\PyStringNode;
-use Behat\Gherkin\Node\TableNode;
 namespace PaulGibbs\WordpressBehatExtension\Context;
 
 use Behat\MinkExtension\Context\MinkContext;
+use PaulGibbs\WordpressBehatExtension\Context\Initializer\WordpressContextInitializer;
 
-//http://www.tentacode.net/10-tips-with-behat-and-mink
-/*abstract whatever extends MinkContext {
-public function __call($method, $parameters)
 class WordpressContext extends MinkContext
 {
-    // we try to call the method on the Page first
-    $page = $this->getSession()->getPage();
-    if (method_exists($page, $method)) {
-        return call_user_func_array(array($page, $method), $parameters);
-    }
+    protected $contextInitializer;
 
-    // we try to call the method on the Session
-    $session = $this->getSession();
-    if (method_exists($session, $method)) {
-        return call_user_func_array(array($session, $method), $parameters);
-    }
-
-    // could not find the method at all
-    throw new \RuntimeException(sprintf(
-        'The "%s()" method does not exist.', $method
-    ));
-}
-}*/
-
-{
     /**
-     * Create a new WordPress website from scratch
+     * Set reference back to this context's initializer.
      *
-     * @Given /^\w+ have|has a vanilla wordpress installation$/
+     * @param array $parameters
      */
-    public function installWordPress(TableNode $table = null)
+    public function setContextInitializer(WordpressContextInitializer $initializer)
     {
-        $name     = "admin";
-        $email    = "an@example.com";
-        $password = "test";
-        $username = "admin";
-
-        if ($table) {
-            $hash     = $table->getHash();
-            $row      = $hash[0];
-            $name     = $row["name"];
-            $username = $row["username"];
-            $email    = $row["email"];
-            $password = $row["password"];
-        }
-
-        $mysqli = new \Mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
-        $value  = $mysqli->multi_query(implode("\n", array(
-            "DROP DATABASE IF EXISTS " . DB_NAME . ";",
-            "CREATE DATABASE " . DB_NAME . ";",
-        )));
-        assertTrue($value);
-
-        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-
-        wp_install($name, $username, $email, true, '', $password);
+        $this->contextInitializer = $initializer;
     }
 }
