b800363d1bd897d90498cfa6c525a758632a79cd
Paul Gibbs <djpaul@gmail.com>
2016-11-15 14:25:08 +0000

    Add driver interface methods

--- src/Driver/DriverInterface.php
+++ src/Driver/DriverInterface.php
@@ -8,4 +8,29 @@ namespace PaulGibbs\WordpressBehatExtension\Driver;
  */
 interface DriverInterface
 {
+    /**
+     * Clear object cache.
+     */
+    public function clearCache();
+
+    /**
+     * Activate a plugin.
+     *
+     * @param string $plugin
+     */
+    public function pluginActivate($plugin);
+
+    /**
+     * Deactivate a plugin.
+     *
+     * @param string $plugin
+     */
+    public function pluginDeactivate($plugin);
+
+    /**
+     * Switch active theme.
+     *
+     * @param string $plugin
+     */
+    public function switchTheme($plugin);
 }
