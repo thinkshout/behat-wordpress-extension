575c3801d1671c51b29bf4fab8be736d0d0c5323
Paul Gibbs <djpaul@gmail.com>
2016-12-11 12:59:26 +0000

    Add "Given there are users: ..." step definition.

--- src/Context/WordpressContext.php
+++ src/Context/WordpressContext.php
@@ -1,6 +1,10 @@
 <?php
 namespace PaulGibbs\WordpressBehatExtension\Context;
 
+use InvalidArgumentException;
+use Behat\Gherkin\Node\TableNode;
+use function PaulGibbs\WordpressBehatExtension\is_wordpress_error;
+
 /**
  * Provides step definitions for creating posts, comments, and terms; managing plugins, themes, and
  * the cache; the database, and much more.
@@ -23,5 +27,32 @@ class WordpressContext extends RawWordpressContext
     public function iAmOnDashboard()
     {
         $this->visitPath('wp-admin/');
+    }
+
+    /**
+     * Add specified user accounts.
+     *
+     * Example: Given there are users:
+     *     | user_login | user_pass | user_email        | role          |
+     *     | admin      | admin     | admin@example.com | administrator |
+     *
+     * @Given /^there are users:/
+     *
+     * @param TableNode $users
+     */
+    public function thereAreUsers(TableNode $users)
+    {
+        foreach ($users->getHash() as $user) {
+            $result = wp_insert_user($user);
+
+            if (is_wordpress_error($result)) {
+                throw new InvalidArgumentException(
+                    sprintf(
+                        'Invalid user information schema: %s',
+                        $result->get_error_message()
+                    )
+                );
+            }
+        }
     }
 }
