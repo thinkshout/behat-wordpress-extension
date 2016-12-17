7bdf10401d589c59a472095e96e23475ca509f03
Paul Gibbs <djpaul@gmail.com>
2016-12-17 17:26:25 +0000

    Add log-in step definition.

--- src/Context/UserContext.php
+++ src/Context/UserContext.php
@@ -3,6 +3,7 @@ namespace PaulGibbs\WordpressBehatExtension\Context;
 
 use Behat\Gherkin\Node\TableNode;
 use function PaulGibbs\WordpressBehatExtension\is_wordpress_error;
+use RuntimeException;
 
 /**
  * Provides step definitions for all things relating to users.
@@ -31,7 +32,6 @@ class UserContext extends RawWordpressContext
      * Log user out.
      *
      * Example: Given I am an anonymous user
-     * Example: Given they are an anonymous user
      *
      * @Given /^(?:I am|they are) an anonymous user/
      */
@@ -39,6 +39,25 @@ class UserContext extends RawWordpressContext
     {
         $this->logOut();
     }
-}
 
-//I am logged in as "walter" with password "test"
+    /**
+     * Log user in.
+     *
+     * Example: Given I am logged in as an admin
+     *
+     * @Given /^(?:I am|they are) logged in as (?:a|an) ([\d\w]+)$/
+     *
+     * @param string $role
+     */
+    public function iAmLoggedInAs($role)
+    {
+        $role  = strtolower($role);
+        $users = $this->getWordpressParameter('users');
+
+        if ($users === null || empty($users[$role])) {
+            throw new RuntimeException("User details for role \"{$role}\" not found.");
+        }
+
+        $this->logIn($users[$role]['username'], $users[$role]['password']);
+    }
+}
