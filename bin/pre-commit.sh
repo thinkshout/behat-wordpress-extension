27a32279b82ed85551a289ebdc8ec8b9dea809ee
Paul Gibbs <djpaul@gmail.com>
2016-12-20 15:20:59 +0000

    Try add phplint to pre-commit

--- bin/pre-commit.sh
+++ bin/pre-commit.sh
@@ -2,6 +2,9 @@ git stash -q --keep-index
 ./bin/phpcs.sh
 RESULT=$?
 git stash pop -q
-
 [ $RESULT -ne 0 ] && exit 1
+
+find ./src -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
+[ $RESULT -ne 0 ] && exit 1
+
 exit 0
