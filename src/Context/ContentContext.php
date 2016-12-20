4f405806a527063e413946352982b932707b5e27
Paul Gibbs <djpaul@gmail.com>
2016-12-20 15:19:45 +0000

    Step definition: Given there are posts

--- src/Context/ContentContext.php
+++ src/Context/ContentContext.php
@@ -6,6 +6,21 @@ namespace PaulGibbs\WordpressBehatExtension\Context;
  */
 class ContentContext extends RawWordpressContext
 {
+    /**
+     * Create content of the fiven type.
+     *
+     * Example: Given there are posts:
+     *     | post_type | post_title | post_content | post_status |
+     *     | page      | Tes Post   | Hello World  | publish     |
+     *
+     * @Given /^(?:there are|there is a) posts?:/
+     *
+     * @param TableNode $posts
+     */
+    public function thereArePosts(TableNode $posts)
+    {
+        foreach ($posts->getHash() as $post) {
+            $this->createContent($post);
+        }
+    }
 }
-
-//there are posts
